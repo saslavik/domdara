a9bd1d06af5e8544309d3eb7c9ed2f426e319109
saslavik <saslavik@gmail.com>
2021-02-26 17:57:16 +0200

    first commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <div class="wrapper-content wrapper-content--fixed">
+      <div class="container">
+        <div class="logo">
+          <img src="../assets/img/Logo.png" alt="">
+          <div class="title">домдара</div>
+        </div>
+        <div class="nav"></div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
