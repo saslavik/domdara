16440628e27515742d422b88ee37121165bca909
saslavik <saslavik@gmail.com>
2021-02-27 16:57:59 +0200

    company added

--- src/components/CompanyHome.vue
+++ src/components/CompanyHome.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="company">
-    companys
+    <div class="company_item" v-for="item in 30" :key="item.title">
+      <img src="../assets/img/Image.jpg" alt="">
+      <div class="company_row"></div>
+      <div class="company_title">Магазин MMZ</div>
+      <div class="company_desc">Жидкий силиконовый чехол для телефона</div>
+      <div class="company_additionalInfo"></div>
+    </div>
   </div>
 </template>
 
@@ -8,3 +14,14 @@
 export default {
 };
 </script>
+
+<style lang="scss" scoped>
+.company {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  &_item {
+    flex: 0 1 204px;
+  }
+}
+</style>
